3b9d7ccc66b8069a54275e4f47063c5c40c698a4
Charlie Humphrey <charliehumphrey@Charlies-MacBook-Pro.local>
2020-04-04 10:52:27 -0700

    updated user model

--- models/users.js
+++ models/users.js
@@ -1,25 +1,32 @@
-
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema
+const bcrypt = require('bcrypt');
+const SALT_ROUNDS = 6;
 
-const userSchema = new Schema({
+const userSchema = new mongoose.Schema({
     username: {type: String, required: true, unique: true},
     password: {type: String, required: true},
 },
 {timestamps: true});
 
-const User = mongoose.model("User", userSchema);
+userSchema.set('toJSON', {
+    transform: function(doc, ret) {
+      // remove the password property when serializing doc to JSON
+      delete ret.password;
+      return ret;
+    }
+  });
 
-model.exports = User;
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+userSchema.pre('save', function(next) {
+    const user = this;
+    if (!user.isModified('password')) return next();
+    // password has been changed - salt and hash it
+    bcrypt.hash(user.password, SALT_ROUNDS, function(err, hash) {
+      if (err) return next(err);
+      // replace the user provided password with the hash
+      user.password = hash;
+      next();
+    });
+  });
 
-const userSchema = new Schema({
-  username: {type: String, required: true, unique: true},
-  password: {type: String, required: true},
-},
- {timestamps: true });
-
-const User = mongoose.model("User", userSchema);
-
-module.exports = User;
+  module.exports = mongoose.model('User', userSchema);
